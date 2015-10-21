5262081fe88e7d47f40ef0bf532a5b1657be5fba
scalakid <alaniit007@gmail.com>
2015-10-21 21:37:48 +1100

    Initial commit

---
+++ src/ScalaForLoop_3.scala
@@ -0,0 +1,19 @@
+/**
+ * Created by ADMIN on 21-Oct-15.
+ * Scala equivalent for nested loop
+ */
+object ScalaForLoop_3 {
+
+  def main(args:Array[String])
+  {
+
+    for(i<-1 to 3;j<-1 to 3) //i represents outer loop and j represents inner loop
+   {
+      print("i= "+i)
+      println("  j= "+j)
+  }
+
+
+  }
+
+}
